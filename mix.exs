3c7035a851a50e648de9681d33b30d5a3699c6e4
Wojciech Kaczmarek <wojtekk@kofeina.net>
2014-08-02 22:37:41 +0200

    Use Elixir 0.15.0; bump ver

--- mix.exs
+++ mix.exs
@@ -3,8 +3,8 @@ defmodule Eastar.Mixfile do
 
   def project do
     [ app: :eastar,
-      version: "0.2.0",
-      elixir: "~> 0.14.3",
+      version: "0.3.0-dev",
+      elixir: "~> 0.15.0",
       deps: deps,
       test_coverage: [tool: ExCoveralls]
     ]
