6d060afa10fbe3d7ee3f351c320a762c597ee2f6
Wojciech Kaczmarek <wojtekk@kofeina.net>
2015-01-30 17:32:03 +0100

    add dialyze & reprise to dev

--- mix.exs
+++ mix.exs
@@ -12,7 +12,8 @@ defmodule Eastar.Mixfile do
 
   # Configuration for the OTP application
   def application do
-    []
+    apps = Mix.env == :dev && [:reprise] || []
+    [applications: apps]
   end
 
   # Returns the list of dependencies in the format:
@@ -20,7 +21,9 @@ defmodule Eastar.Mixfile do
   #
   # To specify particular versions, regardless of the tag, do:
   # { :barbat, "~> 0.1", github: "elixir-lang/barbat" }
-  defp deps do
-    [{:excoveralls, "== 0.3.6",  only: :test}]
-  end
+  defp deps do [
+    {:excoveralls, "== 0.3.6",  only: :test},
+    {:dialyze,     "== 0.1.3",  only: :dev},
+    {:reprise,     "== 0.3.0",  only: :dev},
+  ] end
 end
