71600686eb142fb82febc9d573ee8aebcd4f9b27
Wojciech Kaczmarek <wojtekk@kofeina.net>
2015-02-18 22:39:42 +0100

    split simple geo data to a module

--- test/geo_test.exs
+++ test/geo_test.exs
@@ -10,15 +10,12 @@ defmodule Astar.H.Geo.Test do
   test "some distances from a web script" do
     # see http://www.movable-type.co.uk/scripts/latlong.html
 
-    walcz   = {{53,16,00}, {16,28,00}}
-    poznan  = {{52,24,00}, {16,55,00}}
-    wwa     = {{52,14,00}, {21,01,00}}
-
     assert distance( 
       {{50,03,59}, {05,42,53}},
       {{58,38,39}, {03,04,12}}
       ) |> prec1 == 968.9 
 
+    import Astar.GeoExample
     assert distance(walcz, poznan) |> prec1 == 101.0
     assert distance(walcz, wwa)    |> prec1 == 327.0
     assert distance(poznan, wwa)   |> prec1 == 279.3
