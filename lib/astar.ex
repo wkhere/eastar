f0e409f6255cb86870e92c40df712d9fb9325e5f
Wojciech Kaczmarek <wojciech.kaczmarek@pearson.com>
2015-02-19 19:48:57 +0100

    Revert "low-level loops"
    This reverts commit 6425d101a3ea9de43467ae358e43b7c15b11bbed.

--- lib/astar.ex
+++ lib/astar.ex
@@ -25,12 +25,12 @@ defmodule Astar do
     openmap = HeapMap.new
               |> HeapMap.add(h.(start,goal), start, 0)
 
-    loop(env, goal, {openmap, HashSet.new, HashDict.new})
+    loop(env, goal, openmap, HashSet.new, HashDict.new)
   end
 
-  @spec loop(env, vertex, {HeapMap.t, Set.t, Dict.t}) :: [vertex]
+  @spec loop(env, vertex, HeapMap.t, Set.t, Dict.t) :: [vertex]
 
-  defp loop({nbs, _, _}=env, goal, {openmap, closedset, parents}) do
+  defp loop({nbs, dist, h}=env, goal, openmap, closedset, parents) do
     if HeapMap.empty?(openmap) do []
     else
       {_fx, x, openmap} = HeapMap.pop(openmap)
@@ -40,19 +40,10 @@ defmodule Astar do
 
         closedset = Set.put(closedset, x)
 
-        nbs_loop(env, goal, x, nbs.(x), {openmap, closedset, parents})
-      end
-    end
-  end
+        {openmap,parents} = Enum.reduce nbs.(x), {openmap,parents},
+        fn(y, {openmap,parents}=continue) ->
 
-  defp nbs_loop(env, goal, _x, [], st), do:
-    loop(env, goal, st)
-
-  defp nbs_loop({_, dist, h}=env, goal, x, [y|ys], {openmap, closedset, parents}=st) do
-
-          if Set.member?(closedset, y) do
-            nbs_loop(env, goal, x, ys, st)
-            # ^^ continue loop
+          if Set.member?(closedset, y) do continue
           else
             est_g = HeapMap.get_by_key(openmap,x) + dist.(x,y)
 
@@ -63,22 +54,24 @@ defmodule Astar do
               new_gy = est_g
               fy = h.(y, goal) + new_gy
               nopenmap = openmap |> HeapMap.add(fy, y, new_gy)
-              {nopenmap, closedset, nparents}
+              {nopenmap, nparents}
             end
 
             if gy do
               if est_g < gy do
-                nst = updater.(openmap |> HeapMap.delete(ty, y))
-                nbs_loop(env, goal, x, ys, nst)
+                updater.(openmap |> HeapMap.delete(ty, y))
               else
-                nbs_loop(env, goal, x, ys, st)
+                continue
               end
             else
-              nst = updater.(openmap)
-              nbs_loop(env, goal, x, ys, nst)
+              updater.(openmap)
             end
           end
+        end
 
+        loop(env, goal, openmap, closedset, parents)
+      end
+    end
   end
 
 
