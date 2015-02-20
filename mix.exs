fdea94f6906bfd7ec40a8dbedd7d854ce2d736bf
Wojciech Kaczmarek <wojciech.kaczmarek@pearson.com>
2015-02-20 18:39:37 +0100

    bump to v0.3.1

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eastar.Mixfile do
 
   def project do
     [ app: :eastar,
-      version: "0.3.1-dev",
+      version: "0.3.1",
       elixir: "~> 1.0.2",
       package: package,
       description: description,
