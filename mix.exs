6325860b4495f200de796ba8f000c5c9b8a1e8a7
Wojciech Kaczmarek <wojciech.kaczmarek@pearson.com>
2015-02-20 18:50:39 +0100

    dev ver

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eastar.Mixfile do
 
   def project do
     [ app: :eastar,
-      version: "0.3.1",
+      version: "0.3.2-dev",
       elixir: "~> 1.0.2",
       package: package,
       description: description,
