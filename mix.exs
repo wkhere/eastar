9b1b75f8f39151d9a585f0c0095388f9fdaf69d5
Wojciech Kaczmarek <wojtekk@kofeina.net>
2015-10-13 22:51:56 +0200

    allow Elixir 1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Eastar.Mixfile do
   def project do
     [ app: :eastar,
       version: "0.3.5-dev",
-      elixir: "~> 1.0.2",
+      elixir: "~> 1.0",
       package: package,
       description: description,
       deps: deps,
