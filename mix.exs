35223ce25850834a097d40a807731214d6d20a50
Wojciech Kaczmarek <wojtekk@kofeina.net>
2016-01-25 19:30:51 +0100

    dev ver

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eastar.Mixfile do
 
   def project do
     [ app: :eastar,
-      version: "0.3.8",
+      version: "0.3.9-dev",
       elixir: "~> 1.0",
       package: package,
       description: description,
