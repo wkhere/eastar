ca814de919d0e577ca61c2cd4d125f0278ebccb9
Wojciech Kaczmarek <wojtekk@kofeina.net>
2019-08-30 14:42:13 +0200

    Rel v0.4.3

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eastar.Mixfile do
 
   def project do
     [ app: :eastar,
-      version: "0.4.3-dev",
+      version: "0.4.3",
       package: package(),
       description: description(),
       deps: deps(),
