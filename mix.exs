d30d5bcf7df61b0d90e271862f7c4d5f9e2a096a
Wojciech Kaczmarek <wojtekk@kofeina.net>
2019-08-30 17:14:58 +0200

    dev ver [ci skip]

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Eastar.Mixfile do
 
   def project do
     [ app: :eastar,
-      version: "0.5.0",
+      version: "0.5.1-dev",
       package: package(),
       description: description(),
       deps: deps(),
